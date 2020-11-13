dbc4d8433bf19aa647f1c2a985dbc93ba8bf9ee0
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2020-11-13 14:24:06 +0100

    new commit

--- script/index.js
+++ script/index.js
@@ -5,14 +5,41 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-async function allowCamera() {
-    if ('mediaDevices' in navigator) {
-        const md = navigator.mediaDevices;
-        let stream = await md.getUserMedia({ 
-            audio: false, 
-            video: true
-        });
-    }
-}
+window.addEventListener('load', () => {
+    if( 'mediaDevices' in navigator ) {
+        const startButton = document.querySelector('.video > .start-stream');
+        const stopButton = document.querySelector('.video > .stop-stream');
+        const photoButton = document.querySelector('.profile button');
+        const profilePic = document.querySelector('.profile > img');
 
-allowCamera();
+        let stream;
+        startButton.addEventListener('click', async () => {
+            const md = navigator.mediaDevices;
+            stream = await md.getUserMedia({
+                video: { width: 320, height: 320 }
+            })
+
+            const video = document.querySelector('.video > video');
+            video.srcObject = stream;
+        })
+        stopButton.addEventListener('click', () => {
+            if( stream ) {
+                let tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+            }
+        })
+
+        photoButton.addEventListener('click', async () => {
+            if( !stream )
+                return;
+
+            let tracks = stream.getTracks();
+            let videoTrack = tracks[0];
+            let capture = new ImageCapture(videoTrack);
+            let blob = await capture.takePhoto();
+
+            let imgUrl = URL.createObjectURL(blob);
+            profilePic.src = imgUrl;
+        })
+    }
+})
